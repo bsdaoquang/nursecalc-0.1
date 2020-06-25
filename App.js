727d18b2c2104ab9f230e49cfcc4a7c2e4b4ec2b
bsdaoquang <bsdaoquang@gmail.com>
2020-06-25 18:01:07 +0700

    fix error

--- App.js
+++ App.js
@@ -3,10 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import BMICalc from './formulars/bmicalc'
 import TimeBloodTransfusion from './formulars/time_blood_transfusion'
 import TimeFusion from './formulars/time_fusion'
-import Index from './screens/index'
 
 export default function App() {
   return (
-    <Index />
+    <TimeFusion />
   );
 }
