be8f68ce109c7eeb6a3c31072248d4f746a16a6f
bsdaoquang-laptop <bsdaoquang@gmail.com>
2020-07-10 01:29:13 +0700

    không biết là gì

---
+++ components/intersititialAdmob.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {View, Text, Button} from 'react-native'
+import {styles} from '../styles_global/styles'
+import {AdMobInterstitial,} from 'expo-ads-admob';
+
+export default function IntersititialAdmob({route, navigation}){
+
+	// Display an interstitial
+	const showAdInter = async() => {
+	await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+	await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
+	await AdMobInterstitial.showAdAsync();
+	}
+
+	showAdInter()
+
+	const {itemId} = route.params;
+	const title = itemId
+
+	const moveScreen = () => {
+		setTimeout(() => {navigation.navigate(title)},3000)
+	}
+
+	moveScreen()
+
+	return(
+		<View style={{flex: 1, justifyContent:'center'}}>
+			<Text style={{textAlign: 'center', fontSize: 23}}>Quảng cáo</Text>
+			<Text style={{textAlign: 'center', fontSize: 16}}>Tự động chuyển màn hình</Text>
+		</View>	
+	)
+}
